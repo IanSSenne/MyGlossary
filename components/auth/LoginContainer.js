51f92158a9f6493cc1c9121fc79ce20deb3a027e
IanSSenne <IanSSenne@gmail.com>
2020-04-12 15:22:38 -0700

    feat: 🎸 add fields for future use

--- components/auth/LoginContainer.js
+++ components/auth/LoginContainer.js
@@ -2,10 +2,11 @@ import { useFirebase, isLoaded, isEmpty } from "react-redux-firebase"
 import React, { useState } from "react";
 import IsAuthenticated from "./IsAuthenticated";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-
+import { Card, InputGroup } from "@blueprintjs/core";
 export default function LoginContainer() {
     const firebase = useFirebase();
     const auth = firebase.auth();
+    const [email, setEmail] = useState("");
     const logout = () => {
         firebase.logout();
     };
@@ -26,6 +27,21 @@ export default function LoginContainer() {
                 }}
                 firebaseAuth={auth}
             />
+            <Card>
+
+                <InputGroup
+                    leftIcon="envelope"
+                    onChange={(evt) => setEmail(evt.target.value)}
+                    placeholder="Enter your email..."
+                    value={email}
+                />
+                <InputGroup
+                    placeholder="Enter your password..."
+                    leftIcon="lock"
+                    type="password"
+                />
+            </Card>
+
         </IsAuthenticated>
         <IsAuthenticated>
             <button onClick={logout}>Logout</button>
