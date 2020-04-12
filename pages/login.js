4d63abadbbb120f109324d6c76e49f928025dcf8
IanSSenne <IanSSenne@gmail.com>
2020-04-12 15:23:42 -0700

    feat: 🎸 add @blueprintjs css

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { Wrap } from "../components/wrap";
 import LoginContainer from "../components/auth/LoginContainer";
+// import "../scss/global.scss";
+import "@blueprintjs/core/lib/css/blueprint.css";
+
 function Page() {
   return (
-    <LoginContainer></LoginContainer>
+    <>
+      <LoginContainer></LoginContainer>
+    </>
   );
 }
 export default () => <Wrap><Page></Page></Wrap>
